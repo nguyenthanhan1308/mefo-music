58634031af4ccf7310f63b267d6cf1a3819a4c58
Thanh An~ <sunthanhan1308@gmail.com>
2022-07-22 20:30:18 +0700

    responsive for nitro 5

--- components/Marquee/Marquee.tsx
+++ components/Marquee/Marquee.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from "./Marquee.module.css";
 export default function Marquee(props) {
-    const {isPlaying,nowPlaying, setNowPlaying, playlist} = props;
+    const {nowPlaying, setNowPlaying, playlist} = props;
+    const isPlaying =true;
     const onPortalClick = action => {
         const nowPlayingIndex = playlist.findIndex(p => p.id === nowPlaying.id);
         if (action === "next") {
@@ -15,11 +16,13 @@ export default function Marquee(props) {
         }
     };
     return (
-        <div className={`${styles.marqueeContainer} ${isPlaying ? styles.marqueeShowing : styles.marqueeHidden}`} >
-            <button className={styles.portal} onClick={() => onPortalClick("back")} />
-            <div className={styles.blur} />
-            <span className={`${isPlaying && styles.marquee} ${styles.marqueeText}`}>{nowPlaying.title}</span>
-            <button className={styles.portal} onClick={() => onPortalClick("next")} />
+        <div className={styles.marqueeWrapper}>
+            <div className={`${styles.marqueeContainer} ${isPlaying ? styles.marqueeShowing : styles.marqueeHidden}`} >
+                <button type={"button"} className={styles.portal} onClick={() => onPortalClick("back")} />
+                <div className={styles.blur} />
+                <span className={`${isPlaying && styles.marquee} ${styles.marqueeText}`}>{nowPlaying.title}</span>
+                <button type={"button"}  className={styles.portal} onClick={() => onPortalClick("next")} />
+            </div>
         </div>
     );
 }
