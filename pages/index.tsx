22af0e68a8cbd0dc43feb15afe85c3d3b2a825df
Nguyễn Thành An <sunthanhan1308@gmail.com>
2022-08-04 14:26:51 +0700

    refactor code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { useRouter } from "next/router";
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { useState, useEffect } from 'react';
