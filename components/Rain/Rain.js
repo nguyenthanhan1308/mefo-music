6e47121b05c09bf2336aa7481a4439e9cff4fdda
THANHAN <sunthanhan1308@gmail.com>
2023-08-24 16:29:33 +0700

    [24/8] - change audio file location

--- components/Rain/Rain.js
+++ components/Rain/Rain.js
@@ -9,23 +9,23 @@ import "react-rain-animation/lib/style.css";
 const Rain = (props) => {
 
     return (
-        <div className={styles.rainWrapper}>
-          <audio autoPlay preload="auto" loop>
-                <source src="/audio/rain.mp3" type="audio/mpeg" />
-            </audio>
-          <ReactRain
-            numDrops="300"
-          />
-          <div className={styles.loadingWrapper}>
-            <h1 className={styles.loadingText}>L</h1>
-            <h1 className={styles.loadingText}>O</h1>
-            <h1 className={styles.loadingText}>A</h1>
-            <h1 className={styles.loadingText}>D</h1>
-            <h1 className={styles.loadingText}>I</h1>
-            <h1 className={styles.loadingText}>N</h1>
-            <h1 className={styles.loadingText}>G</h1>
-          </div>
+      <div className={styles.rainWrapper}>
+        <audio autoPlay loop>
+          <source src="rain.mp3" type="audio/mpeg" />
+        </audio>
+        <ReactRain
+          numDrops="300"
+        />
+        <div className={styles.loadingWrapper}>
+          <h1 className={styles.loadingText}>L</h1>
+          <h1 className={styles.loadingText}>O</h1>
+          <h1 className={styles.loadingText}>A</h1>
+          <h1 className={styles.loadingText}>D</h1>
+          <h1 className={styles.loadingText}>I</h1>
+          <h1 className={styles.loadingText}>N</h1>
+          <h1 className={styles.loadingText}>G</h1>
         </div>
+      </div>
     )
 }
 
